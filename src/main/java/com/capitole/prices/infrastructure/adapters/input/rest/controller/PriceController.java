57538d08f14be872701915aef371ed6409c31273
Gustavo Chavarro <137195972+gustavochavarro@users.noreply.github.com>
2026-04-19 01:37:44 -0500

    fixed errors in controller
    Signed-off-by: Gustavo Chavarro <137195972+gustavochavarro@users.noreply.github.com>

--- src/main/java/com/capitole/prices/infrastructure/adapters/input/rest/controller/PriceController.java
+++ src/main/java/com/capitole/prices/infrastructure/adapters/input/rest/controller/PriceController.java
@@ -3,22 +3,27 @@ package com.capitole.prices.infrastructure.adapters.input.rest.controller;
 import com.capitole.prices.application.PriceService;
 import com.capitole.prices.domain.model.Price;
 import com.capitole.prices.domain.model.PriceQuery;
+import com.capitole.prices.domain.port.input.PriceUseCase;
 import com.capitole.prices.infrastructure.adapters.input.rest.mapper.PriceRestMapper;
 import com.capitole.prices.infrastructure.adapters.input.rest.model.PriceResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
 
 @RestController
+@RequestMapping("/price")
 public class PriceController {
 
-    private final PriceService priceService;
+    private final PriceUseCase priceUseCase;
 
-    public PriceController(PriceService priceService) {
-        this.priceService = priceService;
+    public PriceController(PriceUseCase priceUseCase) {
+        this.priceUseCase = priceUseCase;
     }
 
+    @GetMapping
     public ResponseEntity<PriceResponse> getPrice(OffsetDateTime applicationDate, Long productId, Integer brandId) {
         PriceQuery query = PriceQuery.of(
                 applicationDate.toLocalDateTime(),
@@ -26,7 +31,7 @@ public class PriceController {
                 brandId
         );
 
-        Price price = PriceService.execute(query);
+        Price price = priceUseCase.execute(query);
         PriceResponse response = PriceRestMapper.toResponse(price);
 
         return ResponseEntity.ok(response);
